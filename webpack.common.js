da9c4131ba11d9b2c24a0232a577672d2c7882bc
myrizkyprasetya <54502211+myrizkyprasetya@users.noreply.github.com>
2020-06-02 14:14:55 +0700

    Add files via upload

---
+++ webpack.common.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+ 
+module.exports = {
+   entry: "./src/app.js",
+   output: {
+       path: path.resolve(__dirname, "dist"),
+       filename: "bundle.js"
+   },
+   module: {
+       rules: [
+           {
+               test: /\.css$/,
+               use: [
+                   {
+                       loader: "style-loader"
+                   },
+                   {
+                       loader: "css-loader"
+                   }
+               ]
+           },
+           {
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  name: '[path][name].[ext]?[contenthash]',
+                },
+              },
+            ],
+          },
+       ]
+   },
+   plugins: [
+       new HtmlWebpackPlugin({
+           template: "./src/index.html",
+           filename: "index.html"
+       })
+   ]
+}
